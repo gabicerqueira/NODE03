dbb7506a8a7a9a7a24a964b57da224b42fc3e5e6
gabicerqueira <133364342+gabicerqueira@users.noreply.github.com>
2023-10-26 10:24:19 -0300

    Initial commit

---
+++ nome.js
@@ -0,0 +1,5 @@
+let nome = function(a){
+    return a
+}
+
+module.exports = nome
---
+++ tudo.js
@@ -0,0 +1,7 @@
+let idadeFuncao = require ("./idade")
+let estadoFuncao = require ("./estado")
+let nomeFuncao = require ("./nome")
+
+console.log(nomeFuncao("Gabi"));
+console.log(idadeFuncao(17));
+console.log(estadoFuncao("SP"));
